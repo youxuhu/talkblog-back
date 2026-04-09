b62896bc8e17104c1c2b228e458e8f8559e9d94b
youxuhu <huyouxu123@outlook.com>
2026-04-09 22:14:32 +0800

    first commit

---
+++ src/main/java/com/revy/talkblogback/TalkblogBackApplication.java
@@ -0,0 +1,13 @@
+package com.revy.talkblogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkblogBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkblogBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/revy/talkblogback/TalkblogBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revy.talkblogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkblogBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
